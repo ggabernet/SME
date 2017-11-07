e562c1350ed829ea96a10e119a388cfa74432b0f
Gisela Gabernet Garriga <gisela.gabernet@gmail.com>
2017-11-07 16:16:42 +0100

    working on oop SME

--- Vespa_class.py
+++ Vespa_class.py
@@ -81,43 +81,52 @@ def main():
 
     print time.strftime("Run completed, results are stored in %Y-%m-%d-%H%M-vespa_run.txt")
 
+
+def _boxmuller():
+    """
+    The Box-Muller transformation picks a number from a gaussian distribution centered in 0 and with sigma = 1
+    given two randomly generated numbers (i, j) in the interval [0, 1].
+    :return: (float) number picked from normal distribution with expectation 0 and sigma 1.
+    """
+    i = np.random.random_sample(1)  # picks random float [0, 1)
+    j = np.random.random_sample(1)  # picks random float [0, 1)
+    return np.sqrt(-2.0 * np.log10(i)) * np.sin(2.0 * np.pi * j)
+
 class SME():
 
-    def __init__(self, parent, offspring_number, sigma, sigma_strategy, matrix_file, aa_excluded):
+    def __init__(self, parent, offspring_number, n_generations, sigma, sigma_strategy, fitness_function, matrix_file, aa_excluded):
         """
         Initialization of a new generation for a simulated molecular evolution iteration.
-        :param parent: [str] peptide sequence of the parent peptide.
-        :param offspring_number: [int] number of offsprings for the generation.
-        :param sigma: [float] sigma value for the probability of mutation distance.
-        :param sigma_strategy: [str: G/T] sigma strategy for the sigma evolution across generations.
-        :param aa_matrix: [np array] amino acid similarity matrix, when 'grantham' is chosen, .
+        :param parent: {str} peptide sequence of the parent peptide.
+        :param offspring_number: {int} number of offsprings for the generation.
+        :param generations: {int} number of generations.
+        :param sigma: {float} sigma value for the probability of mutation distance.
+        :param sigma_strategy: {str: G/T} sigma strategy for the sigma evolution across generations.
+        :param fitness_function: {} fitness function to use as  offspring selection.
+        :param aa_matrix: {path to matrix file} amino acid similarity matrix, when 'grantham' is chosen, .
         :param aa_excluded: amino acids to exclude in generating the mutations.
         """
+        self.first_parent = parent
         self.parent = parent
-        self.lambd = offspring_number
+        self.off_num = offspring_number
+        self.sigma_start = sigma
         self.sigma = sigma
         self.sigma_strategy = sigma_strategy
+        self.n_generations = n_generations
         self.aa_excluded = aa_excluded
+        self.offsprings = []
+        self.fitness_function = []
 
         if not os.path.exists(matrix_file):
             sys.exit("\n Matrix file does not exist under provided path.")
 
-        self.aa_matrix = np.genfromtxt(matrix_file, delimiter='\t', dtype=float, skip_header=True)
+        aa_matrix = np.genfromtxt(matrix_file, delimiter='\t', dtype=float, skip_header=True)
+        self.aa_matrix = (aa_matrix - np.min(aa_matrix, axis=1) /
+                          (np.max(aa_matrix, axis=1) - np.min(aa_matrix, axis=1))
+
         self.aa_order = np.genfromtxt(matrix_file, delimiter='\t', max_rows=1, dtype=str)
 
-
-    def _boxmuller(self):
-        """
-        The Box-Muller transformation picks a number from a gaussian distribution centered in 0 and with sigma = 1
-        given two randomly generated numbers (i, j) in the interval [0, 1].
-        :return: (float) number picked from normal distribution with expectation 0 and sigma 1.
-        """
-        i = np.random.random_sample(1) # picks random float [0, 1)
-        j = np.random.random_sample(1) # picks random float [0, 1)
-        return np.sqrt(-2.0 * np.log10(i)) * np.sin(2.0 * np.pi * j)
-
-
-    def _mutate(self, parent, sigma, matrixfile, no_aa):
+    def _mutate(self):
         """
         Mutates *parent* sequence according to probabilities derived from the provided matrixfile, which have been scaled
         row-wise and using the boltzmann function with decay parameter sigma in the following way:
@@ -140,30 +149,26 @@ class SME():
         :return: offspring sequence as string, euclidean distance to parent as float.
         """
 
-        # Read grantham matrix
-        mat = np.genfromtxt(matrixfile, delimiter='\t', dtype=float, skip_header=True)
-        aa_order = np.genfromtxt(matrixfile, max_rows=1, dtype=str)
-
-        # Scale row-wise
-        row_max = np.max(mat, axis=1)
-        row_min = np.min(mat, axis=1)
-        mat_scaled = (mat - row_min) / (row_max - row_min)
+        mat_scaled = self.aa_matrix
+        aa_order = self.aa_order
         offspring = []
         dist_aa = 0
 
-        for p in range(len(parent)):
-            i = int(np.where(parent[p] == aa_order)[0]) # Translating the amino acid to a column index in the grantham matrix.
+        for p in range(len(self.parent)):
+            i = int(
+                np.where(self.parent[p] == aa_order)[
+                    0])  # Translating the amino acid to a column index in the grantham matrix.
 
             # Calculate the probability of mutation
-            probs = np.exp((-(mat_scaled[i, :] * mat_scaled[i, :])/(2 * sigma * sigma)) /
-                           np.sum(np.exp(-(mat_scaled[i, :] * mat_scaled[i, :])/(2 * sigma * sigma))))
+            probs = np.exp((-(mat_scaled[i, :] * mat_scaled[i, :]) / (2 * self.sigma * self.sigma)) /
+                           np.sum(np.exp(-(mat_scaled[i, :] * mat_scaled[i, :]) / (2 * self.sigma * self.sigma))))
 
             # Making probability of mutation to undesired aa in "no_aa" always 0.
             mc0 = np.array([1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1.])
-            if len(no_aa) == 1 and no_aa[0] == '0':
+            if len(self.aa_excluded) == 1 and self.aa_excluded[0] == '0':
                 pass
-            elif len(no_aa) >= 1:
-                for aa in no_aa:
+            elif len(self.aa_excluded) >= 1:
+                for aa in self.aa_excluded:
                     idx_aa = int(np.where(aa == aa_order)[0])
                     mc0[idx_aa] = 0
             else:
@@ -177,9 +182,34 @@ class SME():
             offspring.extend(aa_mut)
             dist_aa += (mat_scaled[i, aa_mut_idx]) ** 2
 
-        return ''.join(offspring), np.sqrt(dist_aa)
+            # Choosing offspring sigma.
+            if self.sigma_strategy == "G":  # gaussian strategy with mean parent sigma and SD parent sigma.
+                sigma_off = abs(self.sigma + _boxmuller() * self.sigma)
 
-    def generation(self):
+            elif self.sigma_strategy == "T":  # one-third strategy
+                sigma_off = np.random.choice([0.7 * self.sigma, self.sigma, 1.3 * self.sigma])
+
+            else:
+                sys.exit("Unknown Offspring sigma strategy, please choose from:"
+                         "- G (Gaussian distributed): offspring sigma gaussian distributed with SD seed sigma and "
+                         "centered on seed sigma\n"
+                         "- T (one-third strategy): offspring sigma with probability 1/3 0.7*sigma, "
+                         "with probabiliyt 1/3 sigma, with probability 1/3 1.3 * sigma")
+
+        return ''.join(offspring), np.sqrt(dist_aa), sigma_off
+
+    def iteration(self):
+
+        sigma = self.sigma
+
+        # Mutating to new offspring, calculating distance and sigma.
+        off_generation = []
+        for i in range(self.off_num):
+            off, dist, sigma = self._mutate()
+            off_generation.append((off,dist,sigma))
+        self.offsprings.append(off_generation)
+
+
 
 
 
